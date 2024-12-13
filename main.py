fd1422dcf96c22ae3080f0f75a0f800a7df38797
Carol <anakfig2016@gmail.com>
2024-12-13 09:47:33 -0300

    add: function to upload file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import streamlit as st
+
+def uploaderFile():
+    uploadedFile = st.file_uploader("Escolha um arquivo", ['json'])
+
+    if uploadedFile:
+        
+        stringJson = uploadedFile.getvalue().decode("utf-8")
+        st.json(stringJson)
+
+
+uploaderFile()
