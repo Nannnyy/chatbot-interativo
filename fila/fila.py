2d2d77755bff1c0e576f6ce0dd1a14b424916b0e
Carol <anakfig2016@gmail.com>
2024-12-13 09:32:48 -0300

    add: fila

---
+++ fila/fila.py
@@ -0,0 +1,43 @@
+from .no import No
+
+class Fila:
+
+    def __init__(self):
+        self.tamanho = 0
+        self.inicio = None
+        self.fim = None
+
+    def getTamanho(self):
+        return self.tamanho
+
+    def verificarVazia(self):
+        return (self.tamanho == 0)
+    
+    def enfileirar(self, no):
+        if self.verificarVazia():
+            self.inicio = no
+            self.fim = no
+        else:
+            self.fim.setProximo(no)
+            self.fim = no
+        self.tamanho += 1
+    
+    def desenfileirar(self):
+        if self.verificarVazia():
+            return None
+
+        antigoInicio = self.inicio
+        self.inicio = antigoInicio.getProximo()
+        self.tamanho -= 1
+        return antigoInicio
+
+    def getCabeca(self):
+        return self.inicio
+
+    def imprimirFila(self):
+        no = self.inicio
+        print("Início -> ", end='')
+        while(no is not None):
+            print(f"{no.getValor()}", end=' -> ')
+            no = no.getProximo()
+        print("Fim")
---
+++ fila/no.py
@@ -0,0 +1,17 @@
+class No:
+
+    def __init__(self, valor, proximo=None):
+        self.valor = valor
+        self.proximo = proximo
+    
+    def getValor(self):
+        return self.valor
+    
+    def setValor(self, valor):
+        self.valor = valor
+    
+    def getProximo(self):
+        return self.proximo
+    
+    def setProximo(self, proximo):
+        self.proximo = proximo
